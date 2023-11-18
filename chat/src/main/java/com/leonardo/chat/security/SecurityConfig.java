2050ca5fb96b2970ca4a45ba0bd99f797f3e1777
LeonardoKaftal <leonardokaftal1@gmail.com>
2023-11-18 23:08:07 +0100

    Added Dockerfile for render

--- chat/src/main/java/com/leonardo/chat/security/SecurityConfig.java
+++ chat/src/main/java/com/leonardo/chat/security/SecurityConfig.java
@@ -71,7 +71,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("https://chat-app-ptat.onrender.com"));
         configuration.setAllowedMethods(List.of("GET","POST"));
         configuration.setAllowedHeaders(List.of("Authorization","Content-Type"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
